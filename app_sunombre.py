649b242c31a9c90ded86fd18702ca90f2b629389
Elio Ramos <49961976+elioramospr@users.noreply.github.com>
2025-11-25 10:15:21 -0400

    Add files via upload

---
+++ app_sunombre.py
@@ -0,0 +1,40 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+####################
+## ajustar el layout
+#################### 
+
+st.set_page_config(layout="wide")
+
+
+##################
+## tamaño del plot
+################## 
+
+fig, ax = plt.subplots()
+
+
+#########
+## titulo
+######### 
+
+st.title("Esta es mi primer app...")
+
+
+##############################################
+## esto es para que salga una linea horizontal
+############################################## 
+
+st.divider()
+
+#################
+## datos de covid 
+################# 
+
+df_covid = pd.read_csv("https://raw.githubusercontent.com/elioramosweb/archivo_datos/main/datos_diarios-2022-03-22_10_20_15.csv",parse_dates=['date'])
+
+
