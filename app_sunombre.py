2f9f050e400fb4f336404fb51644b89fbd200492
Elio Ramos <49961976+elioramospr@users.noreply.github.com>
2025-11-25 10:36:32 -0400

    Update app_sunombre.py

--- app_sunombre.py
+++ app_sunombre.py
@@ -10,31 +10,4 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 
-
-##################
-## tamaño del plot
-################## 
-
-fig, ax = plt.subplots()
-
-
-#########
-## titulo
-######### 
-
 st.title("Esta es mi primer app...")
-
-
-##############################################
-## esto es para que salga una linea horizontal
-############################################## 
-
-st.divider()
-
-#################
-## datos de covid 
-################# 
-
-df_covid = pd.read_csv("https://raw.githubusercontent.com/elioramosweb/archivo_datos/main/datos_diarios-2022-03-22_10_20_15.csv",parse_dates=['date'])
-
-
